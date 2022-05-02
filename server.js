9f3a1efffc9bd534781e2b1301643c7ee1174171
sticknein <sticknein@gmail.com>
2022-05-02 14:21:19 -0700

    prod mode path.resolve test 2

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const upload = multer({ dest: 'uploads/' });
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 };
 
